c7b59c7d00b1bf5028f75c1410b941530c91a1fc
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-16 15:44:34 +0200

    feat: :sparkles: Display the list of products from the api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles-for-pages/homePage.css'
+import Products from '../components/ProductList/Products';
+
 const HomePage = () => {
+
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const apiData = await response.json();
+        // console.log(response.json());
+
+        setProducts(apiData.products);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchProducts();
+  }, []);
+  
   return (
     <>
-     <div className='Attention-grabbing'><h1> <span>Shop</span>  smarter, 
-     <span>not</span> harder <br /> <span>Find</span> everything 
-     <span> you</span> need</h1></div>
+      <div className='Attention-grabbing'><h1> <span>Shop</span>  smarter,
+        <span>not</span> harder <br /> <span>Find</span> everything
+        <span> you</span> need</h1></div>
+        {products && products.map(product => (
+        <Products
+          key={product.id}
+          title={product.title}
+          image={product.thumbnail}
+          description={product.description}
+         
+        />
+      ))}
     </>
-   
+
   )
 }
 
