82ce42f211a01f76f66ace89288912c7351b2f12
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-16 21:58:24 +0200

    style: :art: add a simple footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css'
+const Footer = () => {
+  return (
+      <div className="container">
+        <p>© 2023 Your Website. All Rights Reserved.</p>
+      </div>
+  );
+};
+
+export default Footer;
