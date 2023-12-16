0a28770e77a494158697a5157d1011a94f6120de
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-16 22:00:02 +0200

    refactor: remove unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import { RouterProvider , createBrowserRouter } from 'react-router-dom'
-import './App.css'
-import Root from './Root'
-import ErrorPage from './pages/ErrorPage'
-import HomePage from './pages/HomePage'
-import CartPage from './pages/CartPage'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Root from "./Root";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Root/>,
-    errorElement:<ErrorPage/>,
-    children:[
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
       {
-        path:"/",
-        element:<HomePage/>
+        path: "/",
+        element: <HomePage />,
       },
       {
-        path:"/cart",
-        element:<CartPage/>
-      }
-    ]
-  }
-])
+        path: "/cart",
+        element: <CartPage />,
+      },
+    ],
+  },
+]);
 function App() {
-
   return (
-    <RouterProvider router={router}/>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
