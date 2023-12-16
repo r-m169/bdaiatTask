0e8ecd57d56d2a4557177edc99c53301c5d2067c
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-16 14:30:34 +0200

    create Attention-grabbing statement

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles-for-pages/homePage.css'
+const HomePage = () => {
+  return (
+    <>
+     <div className='Attention-grabbing'><h1> <span>Shop</span>  smarter, 
+     <span>not</span> harder <br /> <span>Find</span> everything 
+     <span> you</span> need</h1></div>
+    </>
+   
+  )
+}
+
+export default HomePage
