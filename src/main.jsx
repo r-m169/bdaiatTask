ce07f6b890fc0b2440f344e07d7adf2e082e8928
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-17 21:25:43 +0200

    import add i18n to be global

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css'
+import i18n from './i18n'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
