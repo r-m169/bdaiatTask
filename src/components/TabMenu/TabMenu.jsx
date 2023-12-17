810b8693c29e92301fa0916ff4dd39f724c1c8aa
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-17 21:26:28 +0200

    add tab menu option

---
+++ src/components/TabMenu/TabMenu.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import './tabmenu.css';
+import LanguageSwitcher from '../languageSwitcher/LanguageSwitcher'; 
+
+const TabMenu = () => {
+  const { t } = useTranslation();
+  const [showLanguageSwitcher, setShowLanguageSwitcher] = useState(false);
+
+  const handleItemClick = (option) => {
+    console.log('Clicked:', option);
+    if (option === t('settings')) {
+      setShowLanguageSwitcher(true);
+    } else {
+      setShowLanguageSwitcher(false); 
+    }
+  };
+
+  return (
+    <div className="tab-menu-container">
+      <ul className="dropdown-menu show" aria-labelledby="dropdownMenuButton">
+        <li>
+          <button className="dropdown-item" onClick={() => handleItemClick(t('products'))}>
+            {t('products')}
+          </button>
+        </li>
+        <li>
+          <button className="dropdown-item" onClick={() => handleItemClick(t('settings'))}>
+            {t('settings')}
+          </button>
+        </li>
+      </ul>
+      {showLanguageSwitcher && <LanguageSwitcher />} 
+    </div>
+  );
+};
+
+export default TabMenu;
