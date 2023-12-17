353dc3e24b6faa79090cabc47544effb04f0678b
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-17 22:44:31 +0200

    refactor: :fire: remove styles

--- src/components/ProductList/Product.jsx
+++ src/components/ProductList/Product.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
 import './product.css';
+import { useTranslation } from 'react-i18next';
+import translations from '../../locales/es.json';
+import translationE from '../../locales/en.json';
 
-const Product = ({ title, thumbnail, description, isDarkMode, addToCart }) => {
-  const cardStyles = {
-    width: '18rem',
-    backgroundColor: isDarkMode ? '#212529' : '#fff',
-    color: isDarkMode ? '#fff' : '#333',
-  };
+const translateProductInfo = (productId, language) => {
+  let translatedTitle, translatedDescription;
 
-  const cardBodyStyles = {
-    backgroundColor: isDarkMode ? '#212529' : '#fff',
-    color: isDarkMode ? '#fff' : '#333',
+  if (language === 'es') {
+    translatedTitle = translations.title[productId];
+    translatedDescription = translations.description[productId];
+  } else {
+    translatedTitle = translationE.title[productId];
+    translatedDescription = translationE.description[productId];
+  }
+
+  return {
+    translatedTitle,
+    translatedDescription,
   };
+};
+
+const Product = ({ productId, thumbnail, isDarkMode, addToCart }) => {
+  const { t, i18n } = useTranslation();
+
+  const language = i18n.language || 'en'; 
+
+  const { translatedTitle, translatedDescription } = translateProductInfo(productId, language);
 
   return (
-    <div className="card" style={cardStyles}>
+    <div className="card" style={{ width: '18rem' }}>
       <div className="imageContainer">
         <img src={thumbnail} className="card-img-top" alt="..." />
       </div>
-      <div className="card-body content" style={cardBodyStyles}>
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+      <div className="card-body content">
+        <h5 className="card-title">{translatedTitle}</h5>
+        <p className="card-text">{translatedDescription}</p>
         <div className='btn-container'>
-          <button onClick={addToCart} className="d-flex flex-row justify-content-center  btn-addToCart">Add To Cart</button>
+          <button onClick={addToCart} className="d-flex flex-row justify-content-center btn-addToCart">
+            {t('Add To Cart')}
+          </button>
         </div>
       </div>
     </div>
