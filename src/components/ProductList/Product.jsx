2809e8ea7f71b39170071c0a7ba3dec39eca17f9
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-17 21:27:19 +0200

    refactor

--- src/components/ProductList/Product.jsx
+++ src/components/ProductList/Product.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import './product.css'
+import React from 'react';
+import './product.css';
 
-const Products = ({title , thumbnail , description }) => {
+const Product = ({ title, thumbnail, description, isDarkMode, addToCart }) => {
+  const cardStyles = {
+    width: '18rem',
+    backgroundColor: isDarkMode ? '#212529' : '#fff',
+    color: isDarkMode ? '#fff' : '#333',
+  };
+
+  const cardBodyStyles = {
+    backgroundColor: isDarkMode ? '#212529' : '#fff',
+    color: isDarkMode ? '#fff' : '#333',
+  };
+
   return (
-    <div className="card" style={{ width: '18rem' }}>
-  <img src={thumbnail} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="#" className="btn btn-primary">Add To Cart</a>
-  </div>
-</div>
-  )
-}
+    <div className="card" style={cardStyles}>
+      <div className="imageContainer">
+        <img src={thumbnail} className="card-img-top" alt="..." />
+      </div>
+      <div className="card-body content" style={cardBodyStyles}>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <div className='btn-container'>
+          <button onClick={addToCart} className="d-flex flex-row justify-content-center  btn-addToCart">Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Product;
