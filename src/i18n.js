685867fc96dc1acb5e32c24ee8f9ecd77454d8ef
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-12-17 22:37:30 +0200

    add i180n for translation

---
+++ src/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEN from './locales/en.json'; 
+import translationES from './locales/es.json'; 
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: translationEN },
+      es: { translation: translationES },
+    },
+    lng: 'en', 
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
